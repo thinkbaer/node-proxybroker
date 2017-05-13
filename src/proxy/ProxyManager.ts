492a1769d170fd27e13c2d733607332958e237fc
Robert Kania <cezaryrk@yahoo.de>
2017-05-13 19:22:15 +0200

    work on impl proxy manager

---
+++ src/proxy/ProxyManager.ts
@@ -0,0 +1,39 @@
+
+
+
+import {Judge} from "../judge/Judge";
+import {Storage} from "../storage/Storage";
+import {ProxyResults} from "./ProxyResults";
+import {JudgeResults} from "../judge/JudgeResults";
+
+
+export class ProxyManager {
+
+    private _storage: Storage = null;
+
+    private _judge: Judge = null
+
+    constructor(storage:Storage, judge:Judge){
+        this._storage = storage
+        this._judge = judge
+    }
+
+    find(){
+        //this._storage.connection.
+    }
+
+    async validate(ip:string,port:number):Promise<ProxyResults>{
+        let results:JudgeResults = await this._judge.validate(ip,port)
+
+
+
+
+        return null
+
+    }
+
+
+    save(){
+
+    }
+}
