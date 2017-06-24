a36785bf34b40a1596ff5d8fe56e1e3a7b858f53
Robert Kania <cezaryrk@yahoo.de>
2017-06-24 16:34:48 +0200

    extend domain utils by reverse lookup and local hosts lookup

--- src/utils/DomainUtils.ts
+++ src/utils/DomainUtils.ts
@@ -1,11 +1,29 @@
-
-
 import * as dns from "dns";
+import * as _ from 'lodash'
+
+import {PlatformUtils} from "./PlatformUtils";
+
+if (dns['getServers'] && dns['getServers']().length < 2) {
+    let serversToSet = [
+        '127.0.0.1',
+        '8.8.8.8', '8.8.4.4',   // Google Public DNS
+        '77.88.8.8', '77.88.8.1', // Yandex Basic
+        //'77.88.8.88', '77.88.8.2', // Yandex Safe
+        //'77.88.8.7',  '77.88.8.3', // Yandex Family
+    ];
+    dns.setServers(serversToSet)
+}
+
+
 
 export default class DomainUtils {
 
 
+    static IP_REGEX = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
+    static HOSTS:{host:string,ip:string}[] = []
+
     static domainLookup(domain: string): Promise<{ addr: string, family: string }> {
+
         return new Promise(function (resolve, reject) {
             dns.lookup(domain, function (err, address, family) {
                 if (err) {
@@ -15,5 +33,50 @@ export default class DomainUtils {
                 }
             })
         })
+    }
+
+    static reverse(ip: string, local:boolean = false): Promise<string[]> {
+        if (this.IP_REGEX.test(ip)) {
+
+            return new Promise(function (resolve, reject) {
+                dns.reverse(ip, function (err, hostnames) {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(hostnames)
+                    }
+                })
+            }).then((hostnames:string[]) => {
+                // TODO reload host if not loaded
+                let hosts = _.filter(DomainUtils.HOSTS,{ip:ip})
+                if(!_.isEmpty(hosts)){
+                    hosts.forEach(_x => {
+                        hostnames.unshift(_x.host)
+                    })
+                }
+                return _.uniq(hostnames)
+            })
+        } else {
+            return Promise.resolve([])
+        }
+    }
+
+    static reloadHosts(){
+        this.HOSTS = this.getHostsSync()
+    }
+
+    static getHostsSync() : {host:string,ip:string}[] {
+        let content = PlatformUtils.getHostFileContent()
+        let hosts:{host:string,ip:string}[] = []
+        content.split(/\r?\n/).map(function(x:string) {
+            var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(x)
+            if (matches && matches.length === 3) {
+                matches[2].trim().split(' ').forEach(_x => {
+                    hosts.push({host:_x,ip:matches[1]}); // host:ip
+                })
+            }
+            return null
+        });
+        return hosts
     }
 }
---
+++ test/utils/domain_utils.spec.ts
@@ -0,0 +1,36 @@
+import * as mocha from 'mocha';
+describe('', () => {
+})
+
+
+import {suite, test, slow, timeout, pending} from "mocha-typescript";
+import {expect} from "chai";
+import {inspect} from 'util'
+
+import {ClassLoader} from "../../src/utils/ClassLoader";
+import DomainUtils from "../../src/utils/DomainUtils";
+
+@suite('utils/DomainUtils')
+class DomainUtilsTest {
+
+    @test
+    'host file load'(){
+        let hosts = DomainUtils.getHostsSync()
+        expect(hosts).to.deep.include({host:'localhost',ip:'127.0.0.1'})
+
+        // works only if the hostnames are set, @see .travis.yml
+        expect(hosts).to.deep.include({host:'judge.local',ip:'127.0.0.10'})
+        expect(hosts).to.deep.include({host:'proxy.local',ip:'127.0.0.11'})
+    }
+
+    @test
+    async 'reverse'(){
+        let hosts = await DomainUtils.reverse('127.0.0.10')
+        expect(hosts).to.be.empty
+        DomainUtils.reloadHosts()
+
+        hosts = await DomainUtils.reverse('127.0.0.10')
+        expect(hosts).to.deep.include('judge.local')
+    }
+
+}
