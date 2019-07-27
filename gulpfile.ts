c6eb52274d90725c6eefe6e55c647f6d5f3d9f0f
Robert Kania <cezaryrk@yahoo.de>
2019-07-27 15:59:59 +0200

    build: gulp fix

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,9 +2,11 @@ import * as _ from 'lodash';
 import * as glob from 'glob';
 
 _.concat(
-  glob.sync('node_modules/@typexs/base/gulp/*'),
-  glob.sync('src/gulp/*'),
-  glob.sync('gulp/*'),
+  glob.sync('node_modules/@typexs/base/gulp/*.js'),
+  glob.sync('src/gulp/*.ts'),
+  glob.sync('gulp/*.ts'),
 ).filter(x => !/@types\//.test(x))
-  .map(x => require('./' + x));
+  .map(x => {
+    require('./' + x);
+  });
 
