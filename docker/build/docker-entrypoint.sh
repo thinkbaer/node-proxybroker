a3cb627a49a51a5b7fe8763861564e1864b641f5
Robert Kania <cezaryrk@yahoo.de>
2019-08-11 21:00:06 +0200

    build: test docker compose

--- docker/build/docker-entrypoint.sh
+++ docker/build/docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 # ./docker-entrypoint-db.sh postgres
+if [ ! -f "/proxy-broker/installed" ]; then
+    chown -R postgres:postgres /proxy-broker/data
+    chmod 700 /proxy-broker/data
+    sed -i "s+'/var/lib/postgresql/9.6/main'+'/proxy-broker/data'+g" /etc/postgresql/9.6/main/postgresql.conf
+    sed -i "s+#listen_addresses = 'localhost'+listen_addresses = '*'+g" /etc/postgresql/9.6/main/postgresql.conf
+
+    echo "" > /etc/postgresql/9.6/main/pg_hba.conf
+    echo "local all all  trust" >> /etc/postgresql/9.6/main/pg_hba.conf
+    echo "host all all 127.0.0.1/32 trust" >> /etc/postgresql/9.6/main/pg_hba.conf
+    touch /proxy-broker/installed
+fi
 
 if [ ! -f "/proxy-broker/data/PG_VERSION" ]; then
     /etc/init.d/postgresql stop
@@ -8,14 +19,6 @@ if [ ! -f "/proxy-broker/data/PG_VERSION" ]; then
     chmod 700 /proxy-broker/data
 
     mv /var/lib/postgresql/9.6/main/* /proxy-broker/data
-
-    sed  "s+'/var/lib/postgresql/9.6/main'+'/proxy-broker/data'+g" /etc/postgresql/9.6/main/postgresql.conf > /etc/postgresql/9.6/main/postgresql.2.conf
-    mv /etc/postgresql/9.6/main/postgresql.2.conf /etc/postgresql/9.6/main/postgresql.conf
-
-    echo "" > /etc/postgresql/9.6/main/pg_hba.conf
-    echo "local all all  trust" >> /etc/postgresql/9.6/main/pg_hba.conf
-    echo "host all all 127.0.0.1/32 trust" >> /etc/postgresql/9.6/main/pg_hba.conf
-
 
     /etc/init.d/postgresql start
     su - postgres -c 'createuser proxybroker'
